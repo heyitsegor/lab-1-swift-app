2c2a2bbb22356859fc7705dfb7eb473a451104f6
Egor Novoselov <109018533+heyitsegor@users.noreply.github.com>
2022-09-14 19:54:52 +0300

    Initial swift app template

---
+++ lab-1-swift-app/lab_1_swift_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  lab_1_swift_appApp.swift
+//  lab-1-swift-app
+//
+//  Created by Egor Novoselov on 9/14/22.
+//
+
+import SwiftUI
+
+@main
+struct lab_1_swift_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
